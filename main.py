432da5b03df0b9e8f2f3d6d56b1588b6670351ef
Sudhanshu Kumar <64831792+Sidhudog@users.noreply.github.com>
2021-07-26 18:06:57 +0530

    Added paddles

--- main.py
+++ main.py
@@ -11,3 +11,26 @@ win.tracer(0)
 # scre 
 score_a=0
 score_b=0
+
+
+
+
+
+
+
+# paddle a
+paddle_a=turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.shapesize(stretch_wid=5,stretch_len=1)
+paddle_a.color("white")
+paddle_a.penup()
+paddle_a.goto(-350,0)
+# paddle b
+paddle_b=turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.shapesize(stretch_wid=5,stretch_len=1)
+paddle_b.color("white")
+paddle_b.penup()
+paddle_b.goto(350,0)
